c3fdbbf6dd2fc545fa8fb15a57b974064ecdb575
MelanieLeather <melanie_leather@hotmail.com>
2019-04-24 07:10:13 +0100

    bootstrap cards

--- views/layout1.php
+++ views/layout1.php
@@ -5,7 +5,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Travel Blog</title>
-        <link rel="stylesheet" href="views/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <!--<link rel="stylesheet" href="views/bootstrap-4.1.3-dist/css/bootstrap.min.css">-->
         <link href="views/css/style.css" rel="stylesheet" type="text/css"/>      
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         
@@ -58,13 +59,46 @@
 
         <div id="destination" class="offset">
 
-            <div class="col-12 narrow text-center">
-                <h1>Here are our latest destination articles
+           <div class="col-12"> 
+                <h1 class="text-center">Destinations
                </h1>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <a class="btn btn-secondary btn-md" href="#">Find out more</a>
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-4 mt-4">
+  <div class="card">
+    <img class="card-img-top" src="views/images/croatia.jpg" alt="Lake in Croatia">
+    <div class="card-header"><h3>Top ten Croatian islands</h3></div>
+    <div class="card-body">
+      <a href="#" class="stretched-link card-title">Croatia provides the perfect opportunity for island-hopping. Find your ideal getaway with our top ten islands</a>
+     </div>
+    </div>
+</div>
+ 
+<div class="col-md-4 mt-4">
+  <div class="card">
+    <img class="card-img-top" src="images/croatia.jpg" alt="Lake in Croatia">
+    <div class="card-header">Top ten Croatian islands</div>
+    <div class="card-body text-center">
+      <a href="#" class="stretched-link card-title">Join a Library </a>
+     </div>
+    </div>
+</div>
                 
-            </div>    
+<div class="col-md-4 mt-4">
+  <div class="card">
+    <img class="card-img-top" src="Images/JoinLibrary1.jpg" alt="Library">
+    <div class="card-header">Header</div>
+    <div class="card-body">
+      <a href="#" class="stretched-link card-title">Join a Library </a>
+     </div>
+    </div>
+</div>
+ </div> 
+     
+ </div>    
+               <div class="text-center"><a class="btn btn-secondary btn-md" href="#">Find out more</a></div>
+         
+        </div>
         </div>
         <!--- End destination section --->
 
