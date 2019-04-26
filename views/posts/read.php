3b1fc2a138fc2bbbd550aef45970f52ea8a91188
ghena shaban <ghena@hotmail.co.uk>
2019-04-27 00:46:31 +0100

    working

--- views/posts/read.php
+++ views/posts/read.php
@@ -21,6 +21,45 @@ $(document).ready(function(){
   display: none;
 }
 </style>
+<style>
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
 </body>
 <div id="banner">
 
@@ -53,17 +92,8 @@ echo $img;   }
   echo "<br>";
    echo "likes:";
  echo $likes->totalcount; 
-
-    
-      require_once 'views/posts/addLike.php';
-       require_once 'views/posts/addComment.php';
-      
-   ?>
-
-
-
-
-<div id="flip">Comments</div>
+?>
+ <div id="flip">Comments</div>
 <div id="panel"><p>
     <?php 
    
@@ -75,6 +105,58 @@ foreach ($comments as $comment) { ?>
     ?>
 </p></div>
 </div>
+<?php
+    require_once 'views/posts/addComment.php';
+      require_once 'views/posts/addLike.php';
+      
+      
+   ?>
+
+<button id="myBtn">Comment</button>
+
+
+<div id="myModal" class="modal">
+
  
+  <div class="modal-content">
+    <span class="close">&times;</span>
+ 
+    <h3> it looks like your not logged in </h3>
+    "<a href="?controller=user&action=login"><button>Log in here</button> </a>";
+  </div>
+
+
+
+ <script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+
 </body>
-    </html>
+</html>
+
+
