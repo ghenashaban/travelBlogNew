936dc0727460416c0435eaaf438f96b5b61c0766
MelanieLeather <melanie_leather@hotmail.com>
2019-04-28 18:48:10 +0100

    gallery logo and other CSS changes

--- views/pages/divs.php
+++ views/pages/divs.php
@@ -106,6 +106,9 @@ require_once 'views/pages/homepage-sections.php';
        <script src="views/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
        <script src="https://use.fontawesome.com/releases/v5.5.0/js/all.js"></script>
        <script src="views/JS/cookiealert.js" type="text/javascript"></script>
+       
+              <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <!--- End of Script Source Files -->
 
 
