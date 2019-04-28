130dfc9306b56aea67b87c2a4ef653e7a4deb41d
ghena shaban <ghena@hotmail.co.uk>
2019-04-28 17:37:46 +0100

    live search works but not with mvc

--- views/posts/phpAjax.php
+++ views/posts/phpAjax.php
@@ -1,78 +1,76 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=blog", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      
-   
- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Live MySQL Database Search</title>
+<style type="text/css">
+    body{
+        font-family: Arail, sans-serif;
+    }
+    /* Formatting search box */
+    .search-box{
+        width: 300px;
+        position: relative;
+        display: inline-block;
+        font-size: 14px;
+    }
+    .search-box input[type="text"]{
+        height: 32px;
+        padding: 5px 10px;
+        border: 1px solid #CCCCCC;
+        font-size: 14px;
+    }
+    .result{
+        position: absolute;        
+        z-index: 999;
+        top: 100%;
+        left: 0;
+    }
+    .search-box input[type="text"], .result{
+        width: 100%;
+        box-sizing: border-box;
+    }
+    /* Formatting result items */
+    .result p{
+        margin: 0;
+        padding: 7px 10px;
+        border: 1px solid #CCCCCC;
+        border-top: none;
+        cursor: pointer;
+    }
+    .result p:hover{
+        background: #f2f2f2;
+    }
+</style>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+    $('.search-box input[type="text"]').on("keyup input", function(){
+        /* Get input value on change */
+        var inputVal = $(this).val();
+        var resultDropdown = $(this).siblings(".result");
+        if(inputVal.length){
+            $.get("ajaxSearch.php", {term: inputVal}).done(function(data){
+                // Display the returned data in browser
+                resultDropdown.html(data);
+            });
+        } else{
+            resultDropdown.empty();
+        }
+    });
     
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-?> 
-
-<?php
-
-      
-      
-try {
-
-       $conn= new PDO("mysql:host=$servername;dbname=blog", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-     $stmt = $conn->prepare("select title from post;"); 
-    $stmt->execute();
-
-   
-    $data=$stmt->fetchAll();
-
-   
-foreach ($data as $row) {
-
-    $book=$row['title'];
-  
-       
- 
-
-}   
-}
-catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-?>
-
-
-<?php
-
-
-// get the q parameter from URL
-$q = $_REQUEST["q"];
-
-$hint = "";
-
-// lookup all hints from array if $q is different from "" 
-if ($q !== "") {
-  $q = strtolower($q);
-  $len=strlen($q);
-  foreach($row as $title) {
-    if (stristr($q, substr($title, 0, $len))) {
-      if ($hint === "") {
-        $hint = $title;
-      } else {
-        $hint .= ", $title";
-      }
-    }
-  }
-}
-
-// Output "no suggestion" if no hint was found or output correct values 
-echo $hint === "" ? "no suggestion" : $hint;
-?>
+    // Set search input value on click of result item
+    $(document).on("click", ".result p", function(){
+        $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+        $(this).parent(".result").empty();
+    });
+});
+</script>
+</head>
+<body>
+    <div class="search-box">
+        <input type="text" autocomplete="off" placeholder="Search country..." />
+        <div class="result"></div>
+    </div>
+</body>
+</html>
