74960c6981196acb67ccff92b8c2770591634a89
ghena shaban <ghena@hotmail.co.uk>
2019-04-30 15:09:11 +0100

    i

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -7,44 +7,44 @@ use function models\Exception\logException;
 class PagesController {
 
     public function home() {
-        
-            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-                require_once 'models/post.php';
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+require_once 'models/post.php';
 //            $PopularPostCOM = Post::PopularPostCOM();
 //            $PopularPostLIKE = Post::PopularPostLIKE();
 //            $RecentPosts = Post::RecentPost();
-                $RecentPostDes = Post::RecentPostDes();
-                $RecentPostEco = Post::RecentPostEco();
-                $RecentPostFam = Post::RecentPostFam();
-                $RecentPostIns = Post::RecentPostIns();
-                $RecentPostTip = Post::RecentPostTip();
-                $postLikeDes = Post::postLikeDes();
-                $postLikeEco = Post::postLikeEco();
-                $postLikeFam = Post::postLikeFam();
-                $postLikeIns = Post::postLikeIns();
-                $postLikeTip = Post::postLikeTip();
+            $RecentPostDes = Post::RecentPostDes();
+            $RecentPostEco = Post::RecentPostEco();
+            $RecentPostFam = Post::RecentPostFam();
+            $RecentPostIns = Post::RecentPostIns();
+            $RecentPostTip = Post::RecentPostTip();
+            $postLikeDes=Post::postLikeDes();
+            $postLikeEco=Post::postLikeEco();
+            $postLikeFam=Post::postLikeFam();
+            $postLikeIns=Post::postLikeIns();
+            $postLikeTip=Post::postLikeTip();
+           
+            require_once 'views/pages/home1.php';
+            
+      
+        } else {
+            require_once 'models/post.php';
+            $search = $_POST['search'];
+            $RecentPostDes = Post::RecentPostDes();
+            $RecentPostEco = Post::RecentPostEco();
+            $RecentPostFam = Post::RecentPostFam();
+            $RecentPostIns = Post::RecentPostIns();
+            $RecentPostTip = Post::RecentPostTip();
+            $postLikeDes=Post::postLikeDes();
+            $postLikeEco=Post::postLikeEco();
+            $postLikeFam=Post::postLikeFam();
+            $postLikeIns=Post::postLikeIns();
+            $postLikeTip=Post::postLikeTip();
+           $posts = Post::search($search);
 
-                require_once 'views/pages/home1.php';
-            } else {
-                require_once 'models/post.php';
-                $search = $_POST['search'];
-                $RecentPostDes = Post::RecentPostDes();
-                $RecentPostEco = Post::RecentPostEco();
-                $RecentPostFam = Post::RecentPostFam();
-                $RecentPostIns = Post::RecentPostIns();
-                $RecentPostTip = Post::RecentPostTip();
-                $postLikeDes = Post::postLikeDes();
-                $postLikeEco = Post::postLikeEco();
-                $postLikeFam = Post::postLikeFam();
-                $postLikeIns = Post::postLikeIns();
-                $postLikeTip = Post::postLikeTip();
-                $posts = Post::search($search);
-
-                require_once('views/pages/searchResult.php');
-             
-            } 
-                
-            }
+            require_once('views/pages/searchResult.php');
+     
+        }
+    }
 
   
 
